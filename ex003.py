de0dc61d139f65c3ad3851d146d0653ca8de8825
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-10 17:01:18 -0300

    Update ex003.py

---
+++ ex003.py
@@ -0,0 +1,10 @@
+import math
+print('Área do circulo:')
+
+raio = input('Digite o raio do circulo: ')
+
+raio_float = float(raio)
+
+area = math.pi * raio_float ** 2
+
+print(f'Área do ciculo = {area:.2f}')
