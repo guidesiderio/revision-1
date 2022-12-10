698ba57bf43aedd3867189bdd90be648f6a3fe36
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-10 17:14:05 -0300

    Update ex004.py

---
+++ ex004.py
@@ -0,0 +1,11 @@
+print('O dobro da área de um quadrado:')
+
+lado_quadrado = input('Digite o lado do quadrado: ')
+
+lado_quadrado_float = float(lado_quadrado)
+
+area_quadrado = lado_quadrado_float ** 2
+dobro_area_quadrado = 2 * area_quadrado
+
+print(f'Área do quadrado = {area_quadrado:.1f}')
+print(f'O dobro da área do quadrado = {dobro_area_quadrado:.1f}')
