5b20c48550e04e26c197e20740bff2545f14f19f
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-10 20:43:41 -0300

    Turno em que você estuda

---
+++ ex011.py
@@ -0,0 +1,16 @@
+print('Turno em que você estuda:')
+
+print('M - Matutino')
+print('V - Vespertino')
+print('N - Notuno')
+opcao = input('Digite uma opção: ')
+
+if opcao == 'M':
+    print('Bom dia!')
+elif opcao == 'V':
+    print('Boa tarde!')    
+elif opcao == 'N':
+    print('Boa noite!')
+else:
+    print('Opção inválida!')   
+         
