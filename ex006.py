8dfe16536b5eaa3f27540cb523fe2800e64683f5
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-10 17:27:09 -0300

    Update ex006.py

---
+++ ex006.py
@@ -0,0 +1,9 @@
+print('Fahrenheit para Celsius:')
+
+temperatura_fahrenheit = input('Digite a temperatura em °F: ')
+
+temperatura_fahrenheit_flot = float(temperatura_fahrenheit)
+
+temperatura_celsius = 5 * ((temperatura_fahrenheit_flot - 32) / 9)
+
+print(f'Temperatura em Celsius: {temperatura_celsius:.2f}°C')
