26ccd96acec87a16d848443b91fecdb59e9f3157
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-10 16:56:04 -0300

    Update ex002.py

---
+++ ex002.py
@@ -0,0 +1,9 @@
+print('Conveter metros para centímetros:')
+
+comprimento = input('Digite o comprimento em metros: ')
+
+comprimento_float = float(comprimento)
+
+comprimento_float *= 100
+
+print(f'O comprimento equivale a {comprimento_float} cm')
